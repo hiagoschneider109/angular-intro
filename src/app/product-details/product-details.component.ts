f0818895ee0c5368f171cdc91b27e02aaa5306c8
hiagoschneider109 <hiagoschneider109@icloud.com>
2022-09-12 14:10:51 +0200

    Adding navigation comp.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
@@ -10,6 +10,7 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
+  //@Input() product!: Product;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -19,8 +20,8 @@ export class ProductDetailsComponent implements OnInit {
     const productIdFromRoute = Number(routeParams.get('productId'));
 
     // Find the product that correspond with the id provided in route.
-    this.product = products.find(
-      (product) => product.id === productIdFromRoute
-    );
+    this.product = products.find((prod) => prod.id === productIdFromRoute);
+
+    window.alert(this.product);
   }
 }
